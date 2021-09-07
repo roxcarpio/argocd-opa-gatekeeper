87051870ea0ef5168bf524e0133430ec91fb9c01
Rosa María Carpio López <rmcarpio@atsistemas.com>
2021-09-07 12:16:20 +0200

    argo script changed to wait for secret

--- argo/installer.sh
+++ argo/installer.sh
@@ -44,11 +44,19 @@ PORT_80=$(kubectl get svc argocd-server -n argocd -o jsonpath="{.spec.ports[0].n
 PORT_443=$(kubectl get svc argocd-server -n argocd -o jsonpath="{.spec.ports[1].nodePort}")
 echo "Access Argo using Nodeport $PORT_80 or $PORT_443"
 
-
+SECRET_NAME="argocd-initial-admin-secret"
 read -p "Do you wish to show the argo admin password (y/n)? " yn
 case $yn in
     # [Yy]* ) echo "instala argo"; break;;
-    [Yy]* ) PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) ; echo -e "+++ Argocd credentials:\nUser: admin\nPassword: ${PASS}"; exit;;
+    [Yy]* ) 
+    while [ $? -eq 1 ]
+    do
+    echo "Waiting for the ${SECRET_NAME} secret"
+    sleep 10s
+    kubectl -n argocd get ${SECRET_NAME} test 2> /dev/null
+    done
+    echo "Secret test created"
+    PASS=$(kubectl -n argocd get secret ${SECRET_NAME} -o jsonpath="{.data.password}" | base64 -d) ; echo -e "+++ Argocd credentials:\nUser: admin\nPassword: ${PASS}"; exit;;
     [Nn]* ) exit;;
     * ) echo "Please answer yes(y/Y) or no(n/N).";;
 esac
